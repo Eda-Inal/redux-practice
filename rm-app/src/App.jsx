45d790add3e400ea4922ee34ac08fa4dd8fef3db
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-24 11:10:21 +0300

    added react router home and about page

--- rm-app/src/App.jsx
+++ rm-app/src/App.jsx
@@ -1,23 +1,26 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from './components/Home';
+import About from './components/About';
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter> // BrowserRouter bileşeni eklendi
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+        </nav>
+        <Routes> // Routes bileşeninin yeri doğru
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
     </div>
   );
 }
