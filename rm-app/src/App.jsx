05027a02021fd3f6dc8d56f70fa95175e7dfcd81
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-24 14:49:52 +0300

    created slice, store

--- rm-app/src/App.jsx
+++ rm-app/src/App.jsx
@@ -2,24 +2,18 @@ import logo from './logo.svg';
 import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from './components/Home';
+import Home from './pages/Home';
 import About from './components/About';
 import { Link } from "react-router-dom";
 
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter> // BrowserRouter bileşeni eklendi
-      <div className="App">
-        <nav>
-          <Link to="/">Home</Link>
-          <Link to="/about">About</Link>
-        </nav>
-        <Routes> // Routes bileşeninin yeri doğru
+    <div >
+      <BrowserRouter> 
+        <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
         </Routes>
-      </div>
     </BrowserRouter>
     </div>
   );
---
+++ rm-app/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { UseSelector, useSelector } from 'react-redux'
+function Home() {
+    const data = useSelector((state) => state.characters);
+    console.log(data);
+  return (
+    <div>
+      Home
+    </div>
+  )
+}
+
+export default Home
