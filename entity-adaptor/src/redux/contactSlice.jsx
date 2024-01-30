2287f09bde94a57a249d591a20b7518bb3a27545
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-30 11:23:49 +0300

    add entity adapter

--- entity-adaptor/src/redux/contactSlice.jsx
+++ entity-adaptor/src/redux/contactSlice.jsx
@@ -1,14 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice,createEntityAdapter } from "@reduxjs/toolkit";
 
-
+export const contactAdaptor = createEntityAdapter();
+const initialState = contactAdaptor.getInitialState();
 const contactSlice = createSlice({
     name :'contacts',
-    initialState :{
-
-    },
+    initialState ,
     reducers : {
-
+addContact : contactAdaptor.addOne,
     }
 })
-
+export const {addContact} = contactSlice.actions;
 export default contactSlice.reducer;
