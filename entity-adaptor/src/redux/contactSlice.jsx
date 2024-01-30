2d5861e597c9fb149e5082fcc538e9adc79e3858
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-30 10:37:26 +0300

    created redux store and slice

---
+++ entity-adaptor/src/redux/contactSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+
+const contactSlice = createSlice({
+    name :'contacts',
+    initialState :{
+
+    },
+    reducers : {
+
+    }
+})
+
+export default contactSlice.reducer;
