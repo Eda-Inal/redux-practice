5e40e7c012671691fb83d321f4c00ee7502d23af
Eda Işıl İnal <edainalisil@gmail.com>
2024-01-30 14:21:00 +0300

    add phone number input

--- entity-adaptor/src/components/Contacts/Form.jsx
+++ entity-adaptor/src/components/Contacts/Form.jsx
@@ -1,34 +1,31 @@
 import React from 'react'
 import { useState } from 'react'
-import {useDispatch} from "react-redux";
-import { addContact} from '../../redux/contactSlice';
+import { useDispatch } from "react-redux";
+import { addContact } from '../../redux/contactSlice';
 import { nanoid } from '@reduxjs/toolkit';
 
 function Form() {
-    const [name,setName] = useState('');
-    const dispatch =useDispatch();
-    const handleSubmit =(e) =>{
-e.preventDefault();
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
+    const dispatch = useDispatch();
+    const handleSubmit = (e) => {
+        e.preventDefault();
 
-setName('');
-const names = name.split(",");
+        setName('');
+        const names = name.split(",");
 
-dispatch(addContact({id:nanoid(),name}))
+        dispatch(addContact({ id: nanoid(), name }))
 
     }
-  return (
-    <div>
-      <form  onSubmit={handleSubmit}>
-        <input
-        value={name}
-        onChange={(e) =>setName(e.target.value)}
-        type="text"
-        placeholder='name'
-
-         />
-      </form>
-    </div>
-  )
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input value={name} onChange={(e) => setName(e.target.value)} placeholder='name' />
+                <input placeholder='phone number' value={number} onChange={(e) => setNumber(e.target.value)} />
+                <button type='submit'>Add</button>
+            </form>
+        </div>
+    )
 }
 
 export default Form
